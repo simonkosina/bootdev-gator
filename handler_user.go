db81f96005a64989cc588fba0a805a5f1274450a
skosina <skosina@redhat.com>
2025-08-28 17:49:50 +0200

    register user command

--- handler_user.go
+++ handler_user.go
@@ -1,7 +1,12 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"time"
+
+	"github.com/google/uuid"
+	"github.com/simonkosina/bootdev-blog-aggregator/internal/database"
 )
 
 func handlerLogin(s *state, cmd command) error {
@@ -9,11 +14,49 @@ func handlerLogin(s *state, cmd command) error {
 		return fmt.Errorf("'login' expects a single username argument")
 	}
 
-	if err := s.cfg.SetUser(cmd.args[0]); err != nil {
+	name := cmd.args[0]
+
+	_, err := s.db.GetUser(context.Background(), name)
+	if err != nil {
+		return fmt.Errorf("'login' failed to find user: %w", err)
+	}
+
+	if err := s.cfg.SetUser(name); err != nil {
 		return fmt.Errorf("'login' failed to set user: %w", err)
 	}
 
-	fmt.Printf("User has been set to: %s", cmd.args[0])
+	fmt.Printf("Current user has been set to: %s\n", name)
+
+	return nil
+}
+
+func handlerRegister(s *state, cmd command) error {
+	if len(cmd.args) != 1 {
+		return fmt.Errorf("'register' expects a single username argument")
+	}
+
+	id := uuid.New()
+	currentTime := time.Now().UTC()
+	name := cmd.args[0]
+
+	user, err := s.db.CreateUser(
+		context.Background(),
+		database.CreateUserParams{
+			ID:        id,
+			UpdatedAt: currentTime,
+			CreatedAt: currentTime,
+			Name:      name,
+		})
+
+	if err != nil {
+		return fmt.Errorf("'register' failed to create user: %v", err)
+	}
+
+	if err := s.cfg.SetUser(name); err != nil {
+		return fmt.Errorf("'register' failed to set user: %w", err)
+	}
+
+	fmt.Printf("User was created successfully: %v", user)
 
 	return nil
 }
--- main.go
+++ main.go
@@ -1,24 +1,35 @@
 package main
 
 import (
+	"database/sql"
 	"log"
 	"os"
 
 	"github.com/simonkosina/bootdev-blog-aggregator/internal/config"
+	"github.com/simonkosina/bootdev-blog-aggregator/internal/database"
+
+	_ "github.com/lib/pq"
 )
 
 type state struct {
 	cfg *config.Config
+	db  *database.Queries
 }
 
 func main() {
 	cfg, err := config.Read()
 	if err != nil {
-		log.Fatalf("Error reading config: &v", err)
+		log.Fatalf("Error reading config: %v", err)
+	}
+
+	db, err := sql.Open("postgres", cfg.DbURL)
+	if err != nil {
+		log.Fatalf("Error opening database: %v", err)
 	}
 
 	st := state{
 		cfg: &cfg,
+		db:  database.New(db),
 	}
 
 	cmds := commands{
@@ -26,6 +37,7 @@ func main() {
 	}
 
 	cmds.register("login", handlerLogin)
+	cmds.register("register", handlerRegister)
 
 	if len(os.Args) < 2 {
 		// TODO: Print proper usage with all the commands
